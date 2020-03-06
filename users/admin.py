c645fc61a87626f9152aff8fe8fcb73c79af7992
becca4011 <becca4011@naver.com>
2020-03-06 21:52:38 +0900

    airbnb

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models  # admin.py와 같은 폴더 안에 있는 models.py를 불러옴
 
 # Register your models here.
+
+
+@admin.register(models.User)
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
--- users/models.py
+++ users/models.py
@@ -1,3 +1,54 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+# model : 데이터가 보여지는 모습
+
+
+class User(AbstractUser):  # 상속
+
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRU"))
+
+    # 프로필 사진
+    avatar = models.ImageField(null=True, blank=True)
+    # 성별
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    # 소개
+    bio = models.TextField(default="", blank=True)
+    # 생일
+    birthday = models.DateField(null=True)
+    # 언어
+    langauge = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True
+    )
+    # 화폐(달러/원)
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True
+    )
+    # superhost 여부(true/false)
+    superhost = models.BooleanField(default=False)
+
+    # default : 데이터베이스에 bio를 추가하면 필드가 모두 빈자리이므로 default값을 주어 해결
+    # default값을 만들거나(default = ""), 비어있는 필드를 허용(null = True)
