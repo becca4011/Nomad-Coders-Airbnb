f397886bf8aeca541c865a91a2228b96facce644
becca4011 <becca4011@naver.com>
2020-03-15 17:10:23 +0900

    airbnb

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
+
+
+def all_rooms(request):  # core의 urls.py의 이름과 같아야 함
+    all_rooms = models.Room.objects.all()
+    return render(request, "rooms/home.html", context={"rooms": all_rooms})
+    # templates 안의 html 파일 이름과 같아야 함
+    # 템플릿 안의 이름과 context의 이름이 같아야 함(home.html의 rooms)
