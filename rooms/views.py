9ffae19780ac2ab5f67eb5e7af2cef56508f644f
becca4011 <becca4011@naver.com>
2020-03-17 18:42:41 +0900

    airbnb

--- rooms/views.py
+++ rooms/views.py
@@ -1,31 +1,25 @@
-from math import ceil
-from django.shortcuts import render
+from django.utils import timezone
+from django.views.generic import ListView
 from . import models
 
 # Create your views here.
 
 
-def all_rooms(request):  # core의 urls.py의 이름과 같아야 함
-    page = request.GET.get("page", 1)
-    page = int(page or 1)
-    page_size = 10
+class HomeView(ListView):
 
-    limit = page_size * page
-    offset = limit - page_size
-    all_rooms = models.Room.objects.all()[offset:limit]
-    page_count = ceil(models.Room.objects.count() / page_size)
+    """ HomeView Definition """
 
-    return render(
-        request,
-        "rooms/home.html",
-        context={
-            "rooms": all_rooms,
-            "page": page,
-            "page_count": page_count,
-            "page_range": range(1, page_count),
-        },
-    )
+    model = models.Room
 
-    # [0:10] : 0(offset) ~ 10(limit)번까지 보여줌 / [10:20] : 10 ~ 20번까지 보여줌
-    # templates 안의 html 파일 이름과 같아야 함
-    # 템플릿 안의 이름과 context의 이름이 같아야 함(home.html의 rooms)
+    paginate_by = 10
+    paginate_orphans = 5  # orphans : 마지막 페이지의 객실 수가 5개 이하면 이전 페이지에 마지막 페이지의 객실을 넣음
+    ordering = "created"  # 정렬방식
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        now = timezone.now()
+        context["now"] = now
+
+        return context
