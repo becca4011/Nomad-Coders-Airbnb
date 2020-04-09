79878122a3f23a8aba26c5d85f450478b1717256
becca4011 <becca4011@naver.com>
2020-04-09 22:28:35 +0900

    airbnb

--- users/views.py
+++ users/views.py
@@ -126,11 +126,8 @@ def github_callback(request):
                     try:  # user가 존재할 때
                         user = models.User.objects.get(email=email)
 
-                        if user.login_method == models.User.LOGIN_GITHUB:
-                            # 이미 깃허브 계정으로 user를 생성했을 때
-                            pass
-
-                        else:  # 깃허브로 계정을 생성하지 않은 user가 로그인 시도했을 때
+                        if user.login_method != models.User.LOGIN_GITHUB:
+                            # 깃허브로 계정을 생성하지 않은 user가 로그인 시도했을 때
                             raise GithubException()
 
                     except models.User.DoesNotExist:  # user가 존재하지 않을 때 (입력한 email을 가진 user를 찾을 수 없을 때)
@@ -140,6 +137,7 @@ def github_callback(request):
                             username=email,
                             bio=bio,
                             login_method=models.User.LOGIN_GITHUB,
+                            email_verified=True,
                         )  # 새로운 유저 생성
                         user.set_unusable_password()
                         user.save()
@@ -155,4 +153,78 @@ def github_callback(request):
 
     except GithubException:  # 어떤 일이 일어나면 user를 login으로 돌려보냄
         # 에러 메세지
+        return redirect(reverse("users:login"))
+
+
+def kakao_login(request):
+
+    client_id = os.environ.get("KAKAO_ID")
+    redirect_uri = "http://127.0.0.1:8000/users/login/kakao/callback"
+
+    return redirect(
+        f"https://kauth.kakao.com/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code"
+    )
+
+
+class KakaoException(Exception):
+    pass
+
+
+def kakao_callback(request):
+    try:
+        code = request.GET.get("code")
+
+        client_id = os.environ.get("KAKAO_ID")
+        redirect_uri = "http://127.0.0.1:8000/users/login/kakao/callback"
+
+        token_request = requests.get(
+            f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}&redirect_uri={redirect_uri}&code={code}"
+        )
+
+        token_json = token_request.json()
+        error = token_json.get("error", None)
+
+        if error is not None:
+            raise KakaoException()
+
+        access_token = token_json.get("access_token")
+        profile_request = requests.get(
+            "https://kapi.kakao.com/v2/user/me",
+            headers={"Authorization": f"Bearer {access_token}"},
+        )
+
+        profile_json = profile_request.json()
+        kakao_account = profile_json.get("kakao_account")
+        email = kakao_account.get("email", None)
+
+        if email is None:
+            raise KakaoException()
+
+        profile = kakao_account.get("profile")
+        nickname = profile.get("nickname")
+        profile_image = profile.get("profile_image")
+
+        try:
+            user = models.User.objects.get(email=email)
+
+            if user.login_method != models.User.LOGIN_KAKAO:
+                raise KakaoException()
+
+        except models.User.DoesNotExist:  # user가 존재하지 않을 때
+            user = models.User.objects.create(
+                email=email,
+                username=email,
+                first_name=nickname,
+                login_method=models.User.LOGIN_KAKAO,
+                email_verified=True,
+            )
+
+            user.set_unusable_password()
+            user.save()
+
+        login(request, user)
+
+        return redirect(reverse("core:home"))
+
+    except KakaoException:
         return redirect(reverse("users:login"))
